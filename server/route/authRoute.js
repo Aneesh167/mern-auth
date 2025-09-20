970a8fc3be04796796bb2efa8502721935c8cb84
Aneesh167 <aneeshmehta2005@gmail.com>
2025-09-21 01:25:33 +0530

    update is auth

--- server/route/authRoute.js
+++ server/route/authRoute.js
@@ -19,7 +19,7 @@ authRouter.post("/login", login);
 authRouter.post("/logout", logout);
 authRouter.post("/send-verify-otp", userAuth, sendVerifyOtp);
 authRouter.post("/verify-account", userAuth, verifyEmail);
-authRouter.post("/is-auth", userAuth, isAuthenticated);
+authRouter.get("/is-auth", userAuth, isAuthenticated);
 authRouter.post("/send-reset-otp", SendResetOtp);
 authRouter.post("/reset-password", resetPassword);
 authRouter.post("/verify-reset-otp", verifyResetOtp);
