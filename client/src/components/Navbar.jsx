15b4d1adb96984adefc4e2c173ce14f16a155eac
Aneesh167 <aneeshmehta2005@gmail.com>
2025-09-22 19:10:13 +0530

    Your commit message here

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -69,11 +69,7 @@ const Navbar = () => {
 
       {userData ? (
         <div className="w-8 h-8 flex justify-center items-center bg-gray-200 rounded-full text-gray-800 font-medium cursor-pointer relative user-menu-icon">
-          <span
-         
-            onClick={() => setShowMenu((prev) => !prev)}
-            className="z-index-5 "
-          >
+          <span onClick={() => setShowMenu((prev) => !prev)} className="z-5 ">
             {userData.name && userData.name[0]
               ? userData.name[0].toUpperCase()
               : "U"}
